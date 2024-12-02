39bb88519a94b199920e96974a739ca01c2f03c5
Eddie Xiao <thy.isnis@gmail.com>
2024-12-01 19:25:08 -0500

    Adds day 1 part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn main() {
+fn part1(){
     let contents: String =  fs::read_to_string("./input.txt")
         .expect("File open error");
 
@@ -21,8 +21,38 @@ fn main() {
 
     let mut total_distance: i32 = 0;
     for i in 0..list1.len(){
-        println!("{} and {}", list1[i], list2[i]);
         total_distance += (list1[i] - list2[i]).abs();
     }
     println!("Total Distance: {}", total_distance);
+}
+
+fn part2(){
+
+    let contents: String =  fs::read_to_string("./input.txt")
+        .expect("File open error");
+
+    let lines: Vec<&str> = contents.split("\n").collect();
+    let mut list1: Vec<usize> = Vec::new();
+    let mut list2: Vec<usize> = Vec::new();
+
+    for line in lines{
+        let parsed: Vec<&str> = line.split("   ").collect();
+        if parsed[0].is_empty(){
+            break;
+        }
+        list1.push(parsed[0].parse().expect("Should be a int"));
+        list2.push(parsed[1].parse().expect("Should be a int"));
+    }
+    let mut sim_score: usize= 0;
+    for num in list1{
+        // look inside list2 to find count
+        let count:usize = list2.iter().filter(|&val| *val == num).count();
+        sim_score += count * num;
+    }
+    println!("Sim Score: {}", sim_score);
+}
+
+fn main() {
+    part1();
+    part2();
 }
